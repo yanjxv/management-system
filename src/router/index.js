d223904a5a43cf86eab5c5c699e247335c2885a2
Yanguarzz <16029546+yanguarzz@user.noreply.gitee.com>
2025-08-29 18:27:59 +0800

    commonAside组件静态搭建

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import component from 'element-plus/es/components/tree-select/src/tree-select-option.mjs'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+//制定路由规则
+const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: () => import('@/views/Main.vue'),
+  },
+]
+
+const router = createRouter({
+  //设置路由模式
+  history: createWebHashHistory(),
+  routes,
+})
+
+export default router
