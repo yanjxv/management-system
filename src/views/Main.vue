70c155fcc30d2bdbcf09aad8602cb6b062ec67fe
yanjxv <2675234552@qq.com>
2025-09-08 20:33:35 +0800

    tag静态

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,7 @@
 <script setup>
 import CommonAside from '@/components/CommonAside.vue'
 import CommonHeader from '@/components/CommonHeader.vue'
+import CommonTab from '@/components/CommonTab.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import CommonHeader from '@/components/CommonHeader.vue'
         <el-header class="el-header">
           <common-header />
         </el-header>
+        <common-tab />
         <el-main class="right-main">
           <router-view></router-view>
         </el-main>
